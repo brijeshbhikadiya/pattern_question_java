53eb5f1f873fbb49de26d99f0267e63145933ecc
BRIJESH BHIKADIYA <89252901+brijeshbhikadiya@users.noreply.github.com>
2022-07-13 23:25:51 -0600

    Add files via upload

---
+++ pattern/Pattern26.java
@@ -0,0 +1,33 @@
+class Pattern26
+{
+	public static void main(String args[])
+	{
+		int k;
+		for(int i=1;i<=4;i++)
+		{
+			k=i;
+			for(int j=1;j<=7;j++)
+			{
+				if(j>=5-i&&j<=3+i)
+				{
+					System.out.print(k);
+					if(j<4)
+					{
+						k--;
+					}
+					else
+					{
+						k++;
+					}
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
